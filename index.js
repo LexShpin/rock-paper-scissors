665b9e2eba1c2beb7f6278c2324de4126ddcd96e
lexshpin <lexshpin@gmail.com>
2022-04-16 16:40:13 -0600

    Finished the modals and all the logic

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const scissorsComp = document.querySelector('.scissors-btn-comp');
 const playerScore = document.querySelector('.player__score');
 const computerScore = document.querySelector('.computer__score');
 
+const modalWon = document.querySelector('.modal-won');
+const modalLost = document.querySelector('.modal-lost');
+const overlay = document.querySelector('.overlay');
+
+const playAgainBtns = document.querySelectorAll('.modal__button');
+
 let playerPoints = 0;
 let computerPoints = 0;
 let winner;
@@ -26,9 +32,6 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-
-    console.log(playerSelection);
-    console.log(computerSelection);
 
     switch(playerSelection) {
         case 'rock':
@@ -76,17 +79,14 @@ function playRound(playerSelection, computerSelection) {
     playerScore.textContent = playerPoints;
     computerScore.textContent = computerPoints;
 
-    console.log(playerPoints);
-    console.log(computerPoints);
-
     if (playerPoints == 5) {
-        console.log('Player won!');
-        winner = 'Player';
+        modalWon.style.display = 'flex';
+        overlay.classList.add('overlay-active');
         playerPoints = 0;
         computerPoints = 0;
     } else if (computerPoints == 5) {
-        console.log('Computer won!');
-        winner = 'Computer';
+        modalLost.style.display = 'flex';
+        overlay.classList.add('overlay-active');
         playerPoints = 0;
         computerPoints = 0;
     }
@@ -103,22 +103,12 @@ function removeTransition(e) {
     paperComp.classList.remove('active');
 }
 
-function displayResult() {
-
-    // if winner is Player
-        // display modal You won
-        // make the window less opaque
-        // make the window inactive
-    // else if winner is Computer
-        // display modal You lost
-        // make the window less opaque
-        // make the window inactive
-    
-    // When button Play again is clicked
-        // hide modal
-        // set the window's opacity to 1
-        // make the window active
-        // winner = '';
+function startOver() {
+    modalWon.style.display = 'none';
+    modalLost.style.display = 'none';
+    overlay.classList.remove('overlay-active');
+    playerScore.textContent = playerPoints;
+    computerScore.textContent = computerPoints;
 }
 
 rockPlayer.addEventListener('click', () => playRound('rock', computerPlay()));
@@ -126,5 +116,6 @@ paperPlayer.addEventListener('click', () => playRound('paper', computerPlay()));
 scissorsPlayer.addEventListener('click', () => playRound('scissors', computerPlay()));
 
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
 buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+
+playAgainBtns.forEach(button => button.addEventListener('click', startOver));
