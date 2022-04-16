37b75ba9e3c5f4e4c8786c7c81a4b0358b887cf0
lexshpin <lexshpin@gmail.com>
2022-04-15 18:20:55 -0600

    Added the UI and all eventListeners

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 // Generate user answer func
 // Run a loop, generating answers on each iteration until user's or computer's points are equal to 5
 // On each iteration, generate an answer and compare them, assigning points to the winner
+const rockPlayer = document.querySelector('.rock-btn-player');
+const paperPlayer = document.querySelector('.paper-btn-player');
+const scissorsPlayer = document.querySelector('.scissors-btn-player');
+
+const rockComp = document.querySelector('.rock-btn-comp');
+const paperComp = document.querySelector('.paper-btn-comp');
+const scissorsComp = document.querySelector('.scissors-btn-comp');
+
+const playerScore = document.querySelector('.player__score');
+const computerScore = document.querySelector('.computer__score');
+
 function computerPlay() {
     let options = ['rock', 'paper', 'scissors']
     let chosenOption = Math.round(Math.random()*2)
@@ -9,33 +20,44 @@ function computerPlay() {
     return options[chosenOption]
 }
 
-function userPlay() {
-    let userOption = prompt('rock, paper or scissors?').toLowerCase()
+function userPlay(btn) {
+    let userOption;
+
+    if (btn == 'rock') userOption = 'rock';
+    if (btn == 'paper') userOption = 'paper';
+    if (btn == 'scissors') userOption = 'scissors';
 
     return userOption
 }
 
-let userPoints = 0
-let computerPoints = 0
+rockPlayer.addEventListener('click', () => userPlay('rock'));
+paperPlayer.addEventListener('click', () => userPlay('paper'));
+scissorsPlayer.addEventListener('click', () => userPlay('scissors'));
 
-while (userPoints < 5 && computerPoints < 5) {
-    let computerOption = computerPlay()
-    let userOption = userPlay()
+let userPoints = 0;
+let computerPoints = 0;
 
-    if (computerOption == 'rock' && userOption == 'paper') {
-        userPoints++
-    } else if (computerOption == 'rock' && userOption == 'scissors') {
-        computerPoints++
-    } else if (computerOption == 'paper' && userOption == 'rock') {
-        computerPoints++
-    } else if (computerOption == 'paper' && userOption == 'scissors') {
-        userPoints++
-    } else if (computerOption == 'scissors' && userOption == 'rock') {
-        userPoints++
-    } else if (computerOption == 'scissors' && userOption == 'paper') {
-        computerPoints++
-    }
+playerScore.textContent = userPoints;
+computerScore.textContent = computerPoints;
 
-    console.log(`User points: ${userPoints}`)
-    console.log(`Computer points: ${computerPoints}`)
-}
+// while (userPoints < 5 && computerPoints < 5) {
+//     let computerOption = computerPlay()
+//     let userOption = userPlay()
+
+//     if (computerOption == 'rock' && userOption == 'paper') {
+//         userPoints++
+//     } else if (computerOption == 'rock' && userOption == 'scissors') {
+//         computerPoints++
+//     } else if (computerOption == 'paper' && userOption == 'rock') {
+//         computerPoints++
+//     } else if (computerOption == 'paper' && userOption == 'scissors') {
+//         userPoints++
+//     } else if (computerOption == 'scissors' && userOption == 'rock') {
+//         userPoints++
+//     } else if (computerOption == 'scissors' && userOption == 'paper') {
+//         computerPoints++
+//     }
+
+//     console.log(`User points: ${userPoints}`)
+//     console.log(`Computer points: ${computerPoints}`)
+// }
